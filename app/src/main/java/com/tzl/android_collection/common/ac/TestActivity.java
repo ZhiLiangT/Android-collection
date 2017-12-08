b6e8547a0f4ba34f3d089b23ef019230d950f77c
tzl <15036305782@163.com>
2017-12-08 22:38:17 +0800

    perfrom

---
+++ app/src/main/java/com/tzl/android_collection/common/ac/TestActivity.java
@@ -0,0 +1,15 @@
+package com.tzl.android_collection.common.ac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tzl.android_collection.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
